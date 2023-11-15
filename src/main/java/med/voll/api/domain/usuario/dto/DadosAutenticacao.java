93554cf74230fe98071a5d9a1864586445f7b583
tognatopaulo <paulo.tognato@hotmail.com>
2023-11-15 15:12:56 -0300

    Configurações do JSON Web TOKEN

--- src/main/java/med/voll/api/domain/usuario/dto/DadosAutenticacao.java
+++ src/main/java/med/voll/api/domain/usuario/dto/DadosAutenticacao.java
@@ -1,4 +1,4 @@
-package med.voll.api.controller;
+package med.voll.api.domain.usuario.dto;
 
 public record DadosAutenticacao(String login, String senha) {
 }
---
+++ src/main/java/med/voll/api/infra/security/dto/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package med.voll.api.infra.security.dto;
+
+public record DadosTokenJWT(String token) {
+}
